23ee87673d3a8ee38c5895f5c0846119c66218fb
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-01-25 02:21:36 +0600

    setup all environment and create some page

---
+++ src/Pages/User/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-center bg-amber-600 text-white text-7xl">
+        This is Home page.
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from './../App';
+import Home from "../Pages/User/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
     // children: [
     //   {
     //     path: "contacts/:contactId",
